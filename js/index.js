b6e44c50d3b44ae655f1c62d5e28f8de5ccb479a
chaiquan-J <68234687+chaiquan-J@users.noreply.github.com>
2020-07-15 22:22:28 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,70 +1,56 @@
-// 轮播
-var mySwiper = new Swiper('.mast_swiper', {
-    // autoplay: true,//可选选项，自动滑动
-    autoplay: {
+$(function () {
+  // 当前点击的图片弹出模态框
+  $(".bs-example-modal-lg").on("shown.bs.modal", function (e) {
+    // console.log(e.relatedTarget.src);
+    var thisSrc = e.relatedTarget.src;
+    $(".modo_img").attr("src", thisSrc);
+  });
+
+  $("#myTabs a").click(function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+
+  $('#myTabs a[href="#Wx_applet"]').tab("show"); // Select tab by name
+  $("#myTabs a:first").tab("show"); // Select first tab
+  $("#myTabs a:last").tab("show"); // Select last tab
+  $("#myTabs li:eq(2) a").tab("show"); // Select third tab (0-indexed)
+
+  window.onload = function () {
+    $(".Pers_one").fadeIn(1000);
+    $(".Pers_two").fadeIn(1500);
+    $(".Pers_three").fadeIn(2000);
+    $(".Mast_fade").fadeIn(2500);
+    // 轮播
+    var mySwiper = new Swiper(".mast_swiper", {
+      // autoplay: true,//可选选项，自动滑动
+      autoplay: {
         delay: 2000,
         stopOnLastSlide: false,
         disableOnInteraction: false,
-    },
-    loop: true,
-    speed: 1000,
-    freeMode: true,
-    slidesPerView: 3,
-    centeredSlides: true,
-    slideToClickedSlide: true,
-    freeModeMomentumBounce: false,
-})
-
-$(function () {
-    // 当前点击的图片弹出模态框
-    $('.bs-example-modal-lg').on('shown.bs.modal', function (e) {
-        // console.log(e.relatedTarget.src);
-        var thisSrc = e.relatedTarget.src;
-        $('.modo_img').attr('src', thisSrc);
-    })
-
-    $('#myTabs a').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    })
-
-    $('#myTabs a[href="#Wx_applet"]').tab('show') // Select tab by name
-    $('#myTabs a:first').tab('show') // Select first tab
-    $('#myTabs a:last').tab('show') // Select last tab
-    $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
-})
-
-// 监听页面缩放改变swiper显示个数 未完成
-// function changeWidth() {
-//     let width = document.documentElement.clientWidth;
-//     if (width > 1200) {
-//         mySwiper.params.slidesPerView(4);
-//     } else if (width < 992) {
-//         mySwiper.params.slidesPerView = 4;
-//     } else if (width < 592) {
-//         mySwiper.params.slidesPerView = 2;
-//     }
-//     console.log(mySwiper.params);
-// }
-
-// window.onload = function () {
-//     changeWidth();
-// }
-
-// window.onresize = function () {
-//     changeWidth();
-// }
-
-// $(function () {
-//     $(window).resize(function () {
-//         var width = $(this).width();
-
-//         if (width > 1200) {
-//             mySwiper.slidesPerView == 4;
-//         } else if (width < 992) {
-//             mySwiper.slidesPerView == 4;
-//         } else if (width < 592) {
-//             mySwiper.slidesPerView == 2;
-//         }
-//     });
-// })
+      },
+      // 监听页面缩放 改变显示个数
+      on: {
+        resize: function () {
+          let width = document.documentElement.clientWidth;
+          if (width > 1200) {
+            mySwiper.params.slidesPerView = 4;
+          } else if (width > 992) {
+            mySwiper.params.slidesPerView = 3;
+          } else {
+            mySwiper.params.slidesPerView = 2;
+          }
+        },
+      },
+      loop: true,
+      speed: 1000,
+      freeMode: true,
+      slidesPerView: 4,
+      centeredSlides: true,
+      slideToClickedSlide: true,
+      freeModeMomentumBounce: false,
+    });
+    $(".Proj_fade").fadeIn(3000);
+    $(".foot_fade").fadeIn(3500);
+  }
+});
